d6282675856de473d30b6b9d208a7d5937afba39
Omar Bazeed <omarbazeed@gmail.com>
2022-12-07 00:58:33 +0200

    Adding Tetrominos Shapes And Creating The Func Of Random Tetromino

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Cell from './Cell';
 
+import { randomTetromino } from '../tetrominos';
 
-const Stage = ({stage})=>(
+const Stage = ({stage})=>{
+    
+
+    console.log(randomTetromino())
+
+    
+    return(
     <div className='d-flex flex-wrap text-center' style={{width:'600px'}}> 
     {
-        stage.map(row=> row.map((cell , x)=> <Cell key={x} type={cell[0]} />) )
+        stage.map(row => row.map((cell , x)=> <Cell key={x} type={cell[0]} />) )
     }
+
+
+
     </div>
 )
-
+}
 export default  Stage ;
